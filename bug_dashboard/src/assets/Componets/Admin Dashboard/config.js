deac3f84b5b2e58f996a7087bd852d5b53f74ee8
Mr-Baga08 <91715295+Mr-Baga08@users.noreply.github.com>
2025-03-13 23:32:40 +0530

    Update config.js

--- bug_dashboard/src/assets/Componets/Admin Dashboard/config.js
+++ bug_dashboard/src/assets/Componets/Admin Dashboard/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? "https://vercel.com/mr-baga08s-projects/bug-hunt-platform/H5U7Fdc7Be3aqn5g8yjbwJ4SKEcd" 
+  ? "https://bug-hunt-platform.vercel.app" 
   : "http://localhost:3000/api";
 
 export default API_BASE_URL;
