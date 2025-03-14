2e709105ba14c087662c8ed7c46e15d1133db90b
Mrunal08 <56053695+MrunalPattanaik@users.noreply.github.com>
2025-03-14 11:24:36 +0530

    Update hunter.jsx

--- bug_dashboard/src/assets/Componets/Hunter Dashboard/hunter.jsx
+++ bug_dashboard/src/assets/Componets/Hunter Dashboard/hunter.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import TaskDisplayView from "../../../App/Common/Container/TaskDisplay/TaskDisplayView";
-import Navbar from "../../../App/Common/Container/Appbar/NavBar";
 
 export default function Hunter() {
   return (
-  <div>
-    <TaskDisplayView title={'Project Management Dashboard'}/>
-  </div>);
+    <div>
+      <TaskDisplayView title="Bug Hunter Dashboard" role="hunter"/>
+    </div>
+  );
 }
