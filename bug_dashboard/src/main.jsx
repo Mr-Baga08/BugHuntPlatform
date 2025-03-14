657660468564ee51216ad133e2e3fad2392e86c3
Mr-Baga08 <91715295+Mr-Baga08@users.noreply.github.com>
2025-03-14 12:33:31 +0530

    Update main.jsx

--- bug_dashboard/src/main.jsx
+++ bug_dashboard/src/main.jsx
@@ -1,7 +1,14 @@
+// bug_dashboard/src/main.jsx
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+import { configureAxios } from './assets/Componets/Admin Dashboard/config'
+
+// Configure axios globally
+configureAxios(axios);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
